3aa5527b30e6e81aa94c940d699e071c98145b4b
uminkhant <uminkhanthu@gmail.com>
2022-12-11 11:55:27 +0630

    upd proj

---
+++ 04-Student-registration/src/main/java/com/jdc/mkt/common/FooterServlet.java
@@ -0,0 +1,22 @@
+package com.jdc.mkt.common;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/footer")
+public class FooterServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.getRequestDispatcher("/commons/footer.jsp").include(req, resp);
+	}
+
+}
--- 04-Student-registration/src/main/webapp/commons/footer.jsp
+++ 04-Student-registration/src/main/webapp/commons/footer.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<p>Footer</p>
 </body>
 </html>
